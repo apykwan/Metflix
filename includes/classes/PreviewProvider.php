0bb4db8b5c7a6d59c031bf03142502af48f64101
apykwan <apykwan@hotmail.com>
2025-05-14 04:19:58 -0700

    completed displaying entities' images

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -43,7 +43,23 @@ class PreviewProvider
     ";
   }
 
-  private function getRandomEntity() {
+  public function createEntityPreviewSquare(Entity $entity)
+  {
+    $id = $entity->getId();
+    $thumbnail = $entity->getThumbnail();
+    $name = $entity->getName();
+
+    return "
+      <a href='entity.php?id=$id'>
+        <div class='previewContainer small'>
+          <img src='$thumbnail' title='$name'>
+        </div>
+      </a>
+    ";
+  }
+
+  private function getRandomEntity() 
+  {
     $entity = EntityProvider::getEntities($this->con, null, 1);
     return $entity[0];
   }
